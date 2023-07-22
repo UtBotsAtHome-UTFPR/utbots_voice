4a19e9d6bc7d0b20db5d5f7d24147a08e5405e64
gustavo-fardo <gustavo.fardo@hotmail.com>
2023-07-22 15:57:20 -0300

    added new databases and one node only for nlu

--- nlu_ros/src/nlu_node.py
+++ nlu_ros/src/nlu_node.py
@@ -5,37 +5,53 @@ import re
 import rospy
 from std_msgs.msg import String
 
-class CommandsExtractor:
+class NLUnderstanding:
     def __init__(self):
         # Question-answers dict
-        self.comm_dict = {}
+        self.kword_dict = {}
 
         # Get path
         rp = rospkg.RosPack()
         package_path = rp.get_path('nlu_ros')
-        file_path = package_path + "/assets/stored_commands.csv"
 
         # ROS init
-        rospy.init_node("commands_extractor", anonymous=True)
+        rospy.init_node("nlu_node", anonymous=True)
 
         # Get parameter
-        param_csv = rospy.get_param("~csv")
-        rospy.loginfo(f"[COMM_EXTRACTOR] param_csv: {param_csv}")
-        file_path = package_path + "/assets/" + param_csv
+
+        keyword_databases = ["commands", "qa", "people", "drinks"]
+
+        # /utbots/voice/nlu/param
+        get_commands = rospy.get_param("/commands", True)
+        get_qa = rospy.get_param("/qa", False)
+        get_people = rospy.get_param("/people", False)
+        get_drinks = rospy.get_param("/drinks", False)
+        
+        database_selection = {
+        "commands": get_commands,
+        "qa": get_qa,
+        "people": get_people,
+        "drinks": get_drinks
+        }
+
+        rospy.loginfo("[NLU] selected_databases:")
+        for dbase in keyword_databases:
+            file_path = package_path + "/assets/"
+            if database_selection[dbase] == True:
+                rospy.loginfo(dbase)
+                file_path += dbase + ".csv"
+                rospy.loginfo(f"Reading file: {file_path}")
+                with open(file_path, 'r') as csvfile:
+                    reader = csv.reader(csvfile, delimiter=';')
+                    for row in reader:
+                        self.kword_dict[row[0]] = row[1]
 
         # Subscriber
         rospy.Subscriber("/utbots/voice/stt/whispered", String, self.callback)
 
         # Publisher
-        self.pub = rospy.Publisher("/utbots/task_manager/voice_command", String, queue_size=10)
+        self.pub = rospy.Publisher("/utbots/voice/nlu", String, queue_size=10)
         self.pub_speech = rospy.Publisher("/utbots/voice/tts/robot_speech", String, queue_size=1)
-
-        # Process QuestionsAnswers.csv file
-        rospy.loginfo(f"[COMM_EXTRACTOR] Reading file: {file_path}")
-        with open(file_path, 'r') as csvfile:
-            reader = csv.reader(csvfile, delimiter=';')
-            for row in reader:
-                self.comm_dict[row[0]] = row[1]
         
         # Loop
         rospy.spin()
@@ -44,17 +60,17 @@ class CommandsExtractor:
     def callback(self, msg):
         scores = []
         user_command = str(msg.data)
-        for keywords, stored_command in self.comm_dict.items():                                          
+        for keywords, stored_command in self.kword_dict.items():                                          
             score = self.calculate_score(user_command, keywords)                                  
             scores.append((score, stored_command))                                           
         scores = sorted(scores, key=lambda x: x[0], reverse=True)
         if scores[0][0] <= 0.1:                                                                           
-            rospy.loginfo("[COMM_EXTRACTOR] Sorry, I don't know that command.") 
+            rospy.loginfo("[NLU] Sorry, I did not understand you.") 
             self.pub_speech.publish("Sorry, please repeat.")
         else:
             command = scores[0][1]
             self.pub.publish(command)
-            rospy.loginfo(f"[COMM_EXTRACTOR] Command: {command}") 
+            rospy.loginfo(f"[NLU] Understood: {command}") 
 
     # Calculates score of input command against stored command
     def calculate_score(self, input_command, stored_command):                                               
@@ -65,4 +81,4 @@ class CommandsExtractor:
         return score
 
 if __name__ == "__main__":
-    CommandsExtractor()
+    NLUnderstanding()
