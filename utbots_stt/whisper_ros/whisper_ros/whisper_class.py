6d6af8c0e46bd2502b869ddfd825af3ae6e268fa
ehg2004 <enzoholzmanngaio@alunos.utfpr.edu.br>
2025-07-07 11:44:22 -0300

    Whisper working better (need to be tested)

--- utbots_stt/whisper_ros/whisper_ros/whisper_class.py
+++ utbots_stt/whisper_ros/whisper_ros/whisper_class.py
@@ -9,19 +9,44 @@ CHUNK_LENGHT=15
 BATCH=8
 TIMESTAMPS=False
 SAMPLE_R=16_000
+
+DEFAULT_PAR = {
+                # "max_new_tokens": 448,
+                # "num_beams": 1,
+                "condition_on_prev_tokens": False,
+                # "compression_ratio_threshold": 2.4,
+                # "compression_ratio_threshold": 1.35,  # zlib compression ratio threshold (in token space)
+                # "temperature": (0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
+                "temperature": 0.0,
+                # "logprob_threshold": -1.0,
+                # "no_speech_threshold": 0.8,
+                "return_timestamps": False,
+                "language": "english",          # Force English language
+                "task": "transcribe",      # or "transcribe"
+                # "no_repeat_ngram_size": 3 # Optional: avoid repeating phrases   
+                }
+
+    #             generate_kwargs={
+    #                 "language": "english",          # Force English language
+    #                 "task": "transcribe",      # or "translate"
+    #                 "beam_size": 5,            # Optional: beam search
+    #                 "temperature": 0.0,        # Optional: decoding temperature
+    #                 "no_repeat_ngram_size": 3 # Optional: avoid repeating phrases
+
 class WhisperASR:
-    def __init__(self,model=LV3_t,parameters=None,load_def=True,verbose=False):
+    def __init__(self,model=LV3_t,parameters=DEFAULT_PAR,load_def=True,verbose=False):
         self.model_name = model
         self.model = None
         self.processor = None
         self.pipe = None
         self.verbose=verbose
+        self.generate_kwargs=None
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
         if(load_def==True):
             self.load_model(model,parameters)
 
-    def load_model(self, model_id=LV3_t,parameters=None):
+    def load_model(self, model_id=LV3_t,parameters=DEFAULT_PAR):
         """Load model into memory"""
         try:
             self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
@@ -40,18 +65,10 @@ class WhisperASR:
                 feature_extractor=self.processor.feature_extractor,
                 device=self.device,
                 torch_dtype=self.torch_dtype,
-                # chunk_length_s=CHUNK_LENGHT,
-                return_timestamps=TIMESTAMPS,
-                # stride_length_s=[6, 4],  # Stride for context overlap
-                # stride_length_s=(0.5, 0.5),
-    #             generate_kwargs={
-    #                 "language": "english",          # Force English language
-    #                 "task": "transcribe",      # or "translate"
-    #                 "beam_size": 5,            # Optional: beam search
-    #                 "temperature": 0.0,        # Optional: decoding temperature
-    #                 "no_repeat_ngram_size": 3 # Optional: avoid repeating phrases
-    # }
             )
+
+            self.generate_kwargs=parameters
+            
             print("Model successfully loaded")
         except Exception as e:
             print(f"Error loading model: {str(e)}")
@@ -77,7 +94,7 @@ class WhisperASR:
             # Clear GPU cache if available
             if torch.cuda.is_available():
                 torch.cuda.empty_cache()
-            
+            self.generate_kwargs=DEFAULT_PAR
             # Run garbage collection
             gc.collect()
             print("Model successfully unloaded")
@@ -93,10 +110,28 @@ class WhisperASR:
         try:
             audio_padded = self.audio_padding(audio=audio)
 
-            result=self.pipe(audio_padded,
-                                return_timestamps=TIMESTAMPS,  # Enable timestamps if needed
-                                batch_size=BATCH,  # Adjust based on your memory
-                                # chunk_length_s=CHUNK_LENGHT
+            result=self.pipe(
+                            audio_padded,
+                            # return_timestamps=TIMESTAMPS,  # Enable timestamps if needed
+                            # batch_size=BATCH,  # Adjust based on your memory
+                            # chunk_length_s=CHUNK_LENGHT
+                            # generate_kwargs = {
+                            #     # "max_new_tokens": 448,
+                            #     "num_beams": 1,
+                            #     "condition_on_prev_tokens": False,
+                            #     "compression_ratio_threshold": 1.35,  # zlib compression ratio threshold (in token space)
+                            #     "temperature": (0.0),#(0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
+                            #     "logprob_threshold": -1.0,
+                            #     "no_speech_threshold": 0.6,
+                            #     "return_timestamps": False,
+                            #     "language": "english",          # Force English language
+                            #     "task": "translate",      # or "transcribe"
+                            #     # "beam_size": 5,            # Optional: beam search
+                            #     "temperature": 0.0,        # Optional: decoding temperature
+                            #     "no_repeat_ngram_size": 3 # Optional: avoid repeating phrases
+                            #     },
+                            generate_kwargs = self.generate_kwargs
+
                             )
             print(result["text"])
             return result
@@ -105,9 +140,10 @@ class WhisperASR:
             return None
 
     def audio_padding(self,audio):
+        zero_padding = 30
         #SAMPLE_R : 16kHz
-        if audio.shape[-1] < SAMPLE_R * 5:
-            pad_length = SAMPLE_R * 5 - audio.shape[-1]
+        if audio.shape[-1] < SAMPLE_R * zero_padding:
+            pad_length = SAMPLE_R * zero_padding - audio.shape[-1]
             if(self.verbose):
                 print("audio padded!")
             return(np.pad(audio, (0, pad_length), mode="constant"))
