d3ee93e94b99eeaaaa59622c628a72b2469f1eac
ehg2004 <enzoholzmanngaio@alunos.utfpr.edu.br>
2025-07-08 17:28:20 -0300

    tts path corrected

--- utbots_tts/ros_tts/ros_tts/tts_node.py
+++ utbots_tts/ros_tts/ros_tts/tts_node.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 PATH="/home/ehg2004/utbots_ws/src/utbots_voice/utbots_tts/ros_tts/"
 from std_msgs.msg import String
 from  ros_tts.tts_module_small import SpeechSynthModule
-from ament_index_python.packages import get_packages_with_prefixes
+from ament_index_python.packages import get_package_share_directory
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
@@ -18,28 +18,21 @@ class CoquiTTSActionServer(Node):
 
     def __init__(self):
         super().__init__('coqui_tts_server')
-        # self.package_share_directory = get_package_share_directory('ros_tts')
         
         # self.declare_parameter('speaking_mode', "optimus" , ParameterDescriptor(description='speaking_mode -> string'))
         # self.declare_parameter('language_mode', "en" , ParameterDescriptor(description='language_mode string -> ("en" or "pt-br")'))
         self.declare_parameter('model_name', "tts_models/en/ljspeech/tacotron2-DDC" , ParameterDescriptor(description='speaking_mode -> string'))
         self.declare_parameter('use_cuda', False , ParameterDescriptor(description='use_cuda -> Bool'))
         self.declare_parameter('verbose', False , ParameterDescriptor(description='verbose -> Bool'))
-        self.declare_parameter('package_path', PATH , ParameterDescriptor(description='verbose -> string'))
 
         self.declare_parameter('is_robot_talking', False , ParameterDescriptor(description=''))
         
         cb_group=MutuallyExclusiveCallbackGroup()
 
         self.verbose=self.get_parameter('verbose').get_parameter_value().bool_value
-        self.package_share_directory = self.get_parameter('package_path').get_parameter_value().string_value
-
-        # self.tts_module = SpeechSynthModule(self.package_share_directory,
-        #                                     param_speakingMode=self.get_parameter('speaking_mode').get_parameter_value().string_value,
-        #                                     param_languageMode=self.get_parameter('language_mode').get_parameter_value().string_value,
-        #                                     param_use_cuda=self.get_parameter('use_cuda').get_parameter_value().bool_value,
-        #                                     verbose=self.verbose
-        #                                     )
+        self.package_share_directory = (get_package_share_directory('ros_tts').rsplit('install/')[0]+
+                   "src/utbots_voice/utbots_tts/ros_tts/")
+        
         self.tts_module = SpeechSynthModule(
                 package_path=self.package_share_directory,
                 model_name=self.get_parameter('model_name').get_parameter_value().string_value,
@@ -51,6 +44,7 @@ class CoquiTTSActionServer(Node):
             '/utbots/disable_vad',
             callback_group=cb_group
             )
+        
         # if(self.verbose):
         #     self.get_logger().info("[TTS] Verbose enabled")
         #     self.get_logger().info("[TTS] Model path: {}".format(self.tts_module.param_model_path))
@@ -68,6 +62,8 @@ class CoquiTTSActionServer(Node):
         #         self.tts_module.param_languageMode))
         #     self.get_logger().info("[TTS] Index CSV: {}".format(self.tts_module.csvPath))
         
+
+
         self._action_server = ActionServer(
             self,
             TextToSpeech,
